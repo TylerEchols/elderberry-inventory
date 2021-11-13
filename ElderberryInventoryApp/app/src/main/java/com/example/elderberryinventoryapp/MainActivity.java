5a5d424c24a91853686810de794010da20f0570f
Tyler Echols <tyechols@live.com>
2021-11-13 10:13:41 -0700

    Initial Main Activity gui

--- ElderberryInventoryApp/app/src/main/java/com/example/elderberryinventoryapp/MainActivity.java
+++ ElderberryInventoryApp/app/src/main/java/com/example/elderberryinventoryapp/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.elderberryinventoryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Member variables
+    Spinner categorySpinner; // Spinner is the term for dropdown menu
+
+
+    // Constructor
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Spinner code borrows heavily from https://code.tutsplus.com/tutorials/how-to-add-a-dropdown-menu-in-android-studio--cms-37860
+        categorySpinner = findViewById(R.id.category_spinner); // Dropdown menu for choosing item category
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource
+                (this, R.array.item_categories, android.R.layout.simple_spinner_item);
+                // ArrayAdapter responsible for rendering items in item_categories
+                // to screen when dropdown menu is pressed
+        categorySpinner.setAdapter(adapter); // Bind ArrayAdapter to Spinner
     }
+
 }
